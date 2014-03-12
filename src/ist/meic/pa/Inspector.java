822d8ea620859410ff9294b94852654dd80d0364
Rafael Baltazar <rafael.baltazar@live.com.pt>
2014-03-12 22:29:04 +0000

    Added Inspector.java

---
+++ src/ist/meic/pa/Inspector.java
@@ -0,0 +1,25 @@
+package ist.meic.pa;
+
+/**
+ * The Class Inspector. Can be started from any point of a Java program, 
+ * accepting as argument an object that should be inspected. The inspector 
+ * must be started using the following form: new ist.meic.pa.Inspector().
+ * inspect(object)
+ */
+public class Inspector {
+	
+	/**
+	 * Instantiates a new inspector.
+	 */
+	public Inspector() { }
+	
+	/**
+	 * Presents all the relevant features of the object, namely: The class of 
+	 * the object; The name, type and current value of each of the object's 
+	 * fields; Plus other features we deemed important.
+	 * Then provides a simple read-eval-print interface for further inspection.
+	 */
+	public void inspect(Object object) {
+		;
+	}
+}
