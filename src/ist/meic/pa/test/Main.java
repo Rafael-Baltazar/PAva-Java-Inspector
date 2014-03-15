a4c6f79cc3854f8249ae76bf22a2990cdf8ff6ad
Rafael Baltazar <rafael.baltazar@live.com.pt>
2014-03-15 11:44:34 +0000

    Created a Main class to test the Inspector

---
+++ src/ist/meic/pa/test/Main.java
@@ -0,0 +1,14 @@
+package ist.meic.pa.test;
+
+import ist.meic.pa.Inspector;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new Inspector().inspect(args);
+	}
+
+}
